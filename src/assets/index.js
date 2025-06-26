85e1d0935ef23d84d82072e96041871f08693d9c
Artahacking <faizarta9@gmail.com>
2025-06-26 11:48:59 +0700

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,63 @@
+import logo from "./logo.svg";
+import data from "./data.png";
+import android from "./android.png";
+import next from "./next.png";
+import deve from "./deve.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import kotlin from "./tech/kotlin.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+
+
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import boardify from "./company/boardify.png";
+import tesla from "./company/tesla.png";
+
+import lms from "./lms.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+
+export {
+  logo,
+  data,
+  android,
+  next,
+  deve,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  kotlin,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  meta,
+  shopify,
+  tesla,
+  lms,
+  boardify,
+  jobit,
+  tripguide,
+};
